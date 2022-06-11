9ecf9aaea7e0d71d88eb9dd9ea7f3890f1295b87
Ekaterina <mishelledale@gmail.com>
2022-06-11 15:37:44 +0300

    Task 1: Find the second firuge in three-digit number is completed

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Показываем вторую цифру из трехзначного числа");
+Console.WriteLine("Введите трехзначное число");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number < 100 || Number > 999)
+     Console.WriteLine("Введено неверное число");
+
+else
+    {
+    int sum = Number / 10;
+    int second_figure = sum%10;
+
+    Console.WriteLine("Вторая цифра в " + Number + " - " + second_figure); 
+    }
