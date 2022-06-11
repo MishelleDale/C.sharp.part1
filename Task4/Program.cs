48e798171444a11390c5bb85d49cee140e24e574
Ekaterina <mishelledale@gmail.com>
2022-06-11 16:11:39 +0300

    Task4: Find the weekend by the order of a day is done

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Проверяем, является ли указанный день выходным");
+Console.WriteLine("Введите номер дня недели");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 7 || day < 1) 
+    Console.WriteLine("Введите цифру от 1 до 7");
+    
+if (day == 6 || day == 7)
+   Console.WriteLine("Да, выходной");
+if (day > 0 && day < 6)
+   Console.WriteLine("Нет, не выходной");
