a2bdca8546bb83ebcba4b4e049d4e40e7af94490
Ekaterina <mishelledale@gmail.com>
2022-06-11 15:46:06 +0300

    Task2: Delete the second figure in three-digit number is done

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿int Number = new Random().Next(100, 1000);
+System.Console.WriteLine(Number); // Cheching three-digit number
+
+int Number1 = Number / 100; // first figure
+int Number3 = Number%10; // third figure
+
+System.Console.Write(Number1);
+System.Console.Write(Number3);
