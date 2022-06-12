3cb85daf5ce35c0d30564d9b97f9edf95fd435ef
Ekaterina <mishelledale@gmail.com>
2022-06-12 14:18:11 +0300

    Task3: find the third digit in a number or write about its absent is done

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Указываем третью цифру в заданном числе");
+Console.WriteLine("Введите число");
+
+long Number = Convert.ToInt64(Console.ReadLine()); //задать более длинный интервал чисел
+string NumberA = Convert.ToString(Number); // для дальнейшего извлечения символа через индекс
+
+if ( -100 < Number && Number < 100)
+   Console.WriteLine("В заданном числе третьей цифры нет");
+if ( -100 >= Number)
+  Console.WriteLine(NumberA[3]);
+if (Number >= 100)
+    Console.WriteLine(NumberA[2]);
